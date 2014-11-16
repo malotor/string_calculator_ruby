8f4079039234412523eac2a494e0f63f27166027
Manel <malotor@gmail.com>
2014-11-16 15:49:17 +0100

    Refactoring step 4

--- string_calculator.rb
+++ string_calculator.rb
@@ -1,15 +1,12 @@
 class StringCalculator 
 	def self.add(string)
 		result = 0
-		
-		delimiter = ','
-		if string[0,2] == '//' 
-			delimiter = string[2,1]
-			string = string[3..string.length]
-		end
-
-		string.split(/[#{delimiter}\n]/) .each do |number| result = result + number.to_i end	
-
+		delimiter = self.get_delimiter(string)
+		string.gsub(/\/\/#{delimiter}/, '').split(/[#{delimiter}\n]/).each do |number| result = result + number.to_i end	
 		return result
+	end
+
+	def self.get_delimiter(string)
+		return string[0,2] == '//' ? string[2,1]  : ','
 	end
 end
